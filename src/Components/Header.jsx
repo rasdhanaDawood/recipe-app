0435fdf3278b01655a1f95df247eb6ed703158eb
rasdhana <rasdhana@gmail.com>
2025-05-20 11:14:22 +0300

    logo updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 // import "../App.css";
-import chefClaudeIcon from "/src/assets/chef-claude-icon.png"
+import chefIcon from "/src/assets/cook.jpg"
 
 export default function Header() {
   return (
     <header>
-      <img src={chefClaudeIcon} alt="React Logo" width={40}/>
+      <img src={chefIcon} alt="React Logo" width={40}/>
       <h1>RecipeBook</h1>
     </header>
   );
